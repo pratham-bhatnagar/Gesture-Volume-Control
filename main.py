acfda49c84c472f8a14981f4357efaa845b633fb
Pratham Bhatnagar <84272138+pratham-bhatnagar@users.noreply.github.com>
2021-11-16 02:49:18 +0530

    README.md

--- main.py
+++ main.py
@@ -6,26 +6,25 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
-
+# solution APIs
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
+
+# Volume Control Library Usage 
 devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(
-    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 volRange = volume.GetVolumeRange()
-minVol = volRange[0]
-maxVol = volRange[1]
-volBar = 400
-volPer = 0
+minVol , maxVol , volBar, volPer= volRange[0] , volRange[1], 400, 0
 
-
+# Webcam Setup
 wCam, hCam = 640, 480
 cam = cv2.VideoCapture(0)
 cam.set(3,wCam)
 cam.set(4,hCam)
 
+# Mediapipe Hand Landmark Model
 with mp_hands.Hands(
     model_complexity=0,
     min_detection_confidence=0.5,
@@ -46,7 +45,8 @@ with mp_hands.Hands(
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style()
             )
-            
+
+    # multi_hand_landmarks method for Finding postion of Hand landmarks      
     lmList = []
     if results.multi_hand_landmarks:
       myHand = results.multi_hand_landmarks[0]
@@ -55,12 +55,14 @@ with mp_hands.Hands(
         cx, cy = int(lm.x * w), int(lm.y * h)
         lmList.append([id, cx, cy])          
 
+    # Assigning variables for Thumb and Index finger position
     if len(lmList) != 0:
       x1, y1 = lmList[4][1], lmList[4][2]
       x2, y2 = lmList[8][1], lmList[8][2]
 
-      cv2.circle(image, (x1,y1),15,(255,255,255))  #thumb
-      cv2.circle(image, (x2,y2),15,(255,255,255))  # index 
+      # Marking Thumb and Index finger
+      cv2.circle(image, (x1,y1),15,(255,255,255))  
+      cv2.circle(image, (x2,y2),15,(255,255,255))   
       cv2.line(image,(x1,y1),(x2,y2),(0,255,0),3)
       length = math.hypot(x2-x1,y2-y1)
       if length < 50:
@@ -70,18 +72,13 @@ with mp_hands.Hands(
       volume.SetMasterVolumeLevel(vol, None)
       volBar = np.interp(length, [50, 220], [400, 150])
       volPer = np.interp(length, [50, 220], [0, 100])
+
+      # Volume Bar
       cv2.rectangle(image, (50, 150), (85, 400), (0, 0, 0), 3)
       cv2.rectangle(image, (50, int(volBar)), (85, 400), (0, 0, 0), cv2.FILLED)
       cv2.putText(image, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
                 1, (0, 0, 0), 3)
-
-
-
-
-      
-      
-      
-      
+    
     cv2.imshow('handDetector', image) 
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
